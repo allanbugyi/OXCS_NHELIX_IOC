6b8b1c65f83c899a4267ed02ac865ad35a089985
Allan S. B. Bugyi <allan.buyi@lnls.br>
2018-07-30 15:22:03 -0300

    Creating repo

---
+++ OXCS700App/src/O.linux-x86_64/alarmlookup.d
@@ -0,0 +1,18 @@
+alarmlookup.o: ../alarmlookup.c /usr/local/epics/base/include/dbDefs.h \
+ /usr/local/epics/base/include/registryFunction.h \
+ /usr/local/epics/base/include/shareLib.h \
+ /usr/local/epics/synApps_5_8/support/genSubV1-8/include/genSubRecord.h \
+ /usr/local/epics/base/include/epicsTypes.h \
+ /usr/local/epics/base/include/link.h \
+ /usr/local/epics/base/include/dbDefs.h \
+ /usr/local/epics/base/include/epicsMutex.h \
+ /usr/local/epics/base/include/epicsAssert.h \
+ /usr/local/epics/base/include/os/Linux/osdMutex.h \
+ /usr/local/epics/base/include/ellLib.h \
+ /usr/local/epics/base/include/epicsTime.h \
+ /usr/local/epics/base/include/epicsTypes.h \
+ /usr/local/epics/base/include/os/Linux/osdTime.h \
+ /usr/local/epics/base/include/epicsExport.h \
+ /usr/local/epics/base/include/shareLib.h \
+ /usr/local/epics/base/include/alarm.h \
+ /usr/local/epics/base/include/recGbl.h
